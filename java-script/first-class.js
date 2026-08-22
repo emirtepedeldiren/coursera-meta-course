afcc47d79bd1310362fc01c1e0736ac4c6e73e42
emirtepedeldiren <emir.tepedeldiren@gmail.com>
2026-08-22 18:48:02 +0300

    22.08 javascript baslandi

---
+++ java-script/first-class.js
@@ -0,0 +1,12 @@
+var person;
+var person = "Emir";
+console.log("Hello", person)
+
+var greeting = "Hello";
+console.log(greeting,person)
+
+var person = "emir";
+var greeting = "selam";
+
+console.log(greeting,person)
+
